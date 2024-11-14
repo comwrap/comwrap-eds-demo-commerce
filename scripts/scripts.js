a05ab5f81c41f1c7cbc1927ccc6eacfeee01f3a7
Artem Kozynets <akozinets@comwrap.com>
2024-11-14 17:57:41 +0100

    fix: auto buildHeroBlock

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -72,11 +72,15 @@ const pluginContext = {
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
   const picture = main.querySelector('picture');
+  const h1MainParent = h1.parentElement.parentElement;
+  const pictureMainParent = picture.parentElement.parentElement.parentElement;
   // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
+  if(h1MainParent.tagName === 'MAIN' && pictureMainParent.tagName === 'MAIN') {
+    if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
+      const section = document.createElement('div');
+      section.append(buildBlock('hero', { elems: [picture, h1] }));
+      main.prepend(section);
+    }
   }
 }
 
@@ -124,7 +128,9 @@ function buildAutoBlocks(main) {
 // eslint-disable-next-line import/prefer-default-export
 export function decorateMain(main) {
   // hopefully forward compatible button decoration
-  decorateButtons(main);
+  setTimeout(() => {
+    decorateButtons(main);
+  }, 0);
   decorateIcons(main);
   buildAutoBlocks(main);
   decorateSections(main);
